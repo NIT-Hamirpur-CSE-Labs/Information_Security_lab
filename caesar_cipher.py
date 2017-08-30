61f2fb2e5139c64897ef14e17f16ea9a82b29e67
Rishabh <rishabh0402@gmail.com>
2017-08-30 10:20:58 +0530

    Create caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,44 @@
+# code-master5
+print("This program implements Caesar Cipher.")
+print("Select option:")
+print("1. Encrypt a message")
+print("2. Decrypt a message")
+print("Enter 1 or 2:")
+option = int(input())
+
+def encryption_function(key, val):
+    if (val == ' '):
+        return val
+    return chr(((ord(val) - 97 + key) % 26) + 97)
+
+def decryption_function(key, val):
+    if (val == ' '):
+        return val
+    return chr(((ord(val) - 97 - key) % 26) + 97)
+
+if (option == 1):
+    print("Enter a message to encrypt (a-z only):")
+    message = input()
+    print("Enter a key to encrypt (integer only):")
+    key = int(input())
+    message = list(message)
+    encrypted_message = []
+    for i in range(0, len(message)):
+        encrypted_message.append(encryption_function(key, message[i]))
+    encrypted_message = ''.join(encrypted_message)
+    print("Encrypted Message: " + encrypted_message)
+
+elif (option == 2):
+    print("Enter a message to decrypt (a-z only):")
+    encrypted_message = input()
+    print("Enter a key to decrypt (integer only):")
+    key = int(input())
+    encrypted_message = list(encrypted_message)
+    message = []
+    for i in range(0, len(encrypted_message)):
+        message.append(decryption_function(key, encrypted_message[i]))
+    message = ''.join(message)
+    print("Decrypted Message: " + message)
+
+else:
+    print("Invalid Option!")
