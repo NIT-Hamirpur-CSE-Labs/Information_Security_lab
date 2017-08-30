da386d91f9b84a57ac2b0e2ffc1e81f5641fa256
Rishabh <rishabh0402@gmail.com>
2017-08-30 10:22:24 +0530

    Create vignere_cipher.py

---
+++ vignere_cipher.py
@@ -0,0 +1,48 @@
+# code-master5
+print("This program implements Vignere Cipher.")
+print("Select option:")
+print("1. Encrypt a message")
+print("2. Decrypt a message")
+print("Enter 1 or 2:")
+option = int(input())
+
+def encryption_function(key, val):
+    if (val == ' '):
+        return val
+    return chr(((ord(val) - 97 + ord(key)) % 26) + 97)
+
+def decryption_function(key, val):
+    '''
+    if (val == ' '):
+        return val
+    '''
+    return chr(((ord(val) - 97 - ord(key)) % 26) + 97)
+
+if (option == 1):
+    print("Enter a message to encrypt (a-z only):")
+    message = input()
+    message = list(message)
+    print("Enter a keyword to encrypt (a-z only):")
+    key = input()
+    key = list(key)
+    encrypted_message = []
+    for i in range(0, len(message)):
+        encrypted_message.append(encryption_function(key[i % len(key)], message[i]))
+    encrypted_message = ''.join(encrypted_message)
+    print("Encrypted Message: " + encrypted_message)
+
+elif (option == 2):
+    print("Enter a message to decrypt (a-z only):")
+    encrypted_message = input()
+    encrypted_message = list(encrypted_message)
+    print("Enter a key to decrypt (integer only):")
+    key = input()
+    key = list(key)
+    message = []
+    for i in range(0, len(encrypted_message)):
+        message.append(decryption_function(key[i % len(key)], encrypted_message[i]))
+    message = ''.join(message)
+    print("Decrypted Message: " + message)
+
+else:
+    print("Invalid Option!")
