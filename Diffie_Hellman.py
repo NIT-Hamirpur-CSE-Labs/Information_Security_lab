f1430643791f43eb8655d77b4577d5e90ed69310
Rishabh Chaudhary <rishabh0402@gmail.com>
2019-01-14 16:19:05 +0530

    Update Diffie_Hellman.py

--- Diffie_Hellman.py
+++ Diffie_Hellman.py
@@ -3,14 +3,22 @@ import base64
 import hashlib
 import sys
 
+#g - shared value
 g=9
+
+#p - prime number
 p=1001
 
+#a - Alice random number calculation
 a=random.randint(5, 10)
 
+#b - Bob random number calculation
 b=random.randint(10,20)
 
+#A - Alice calculated value
 A = (g**a) % p
+
+#B - Bob calculated value
 B = (g**b) % p
 
 print 'g: ',g,' (a shared value), n: ',p, ' (a prime number)'
@@ -25,10 +33,10 @@ print 'Bob value (B): ',B,' (g^b) mod p'
 
 print '\nAlice calculates:'
 keyA=(B**a) % p
-print 'Key: ',keyA,' (B^a) mod p'
+print 'Key A: ',keyA,' (B^a) mod p'
 print 'Key: ',hashlib.sha256(str(keyA)).hexdigest()
 
 print '\nBob calculates:'
 keyB=(A**b) % p
-print 'Key: ',keyB,' (A^b) mod p'
+print 'Key B: ',keyB,' (A^b) mod p'
 print 'Key: ',hashlib.sha256(str(keyB)).hexdigest()
